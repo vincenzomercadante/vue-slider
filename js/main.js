bb9ceaca9935358f42efc2d0f28f82223da3aa86
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-01-30 16:14:59 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// recover of createApp method
+const { createApp } = Vue;
+
+// app creation
+const app = createApp({
+  data() {
+    return {
+      allImages: ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"],
+      activeImage: 0,
+      interval: false,
+    };
+  },
+
+  methods: {
+    goToSlide(index) {
+      if (this.activeImage === this.allImages.length) {
+        this.activeImage = 0;
+      } else if (this.activeImage < 0) {
+        this.activeImage = this.allImages.length - 1;
+      }
+    },
+  },
+
+  mounted() {},
+});
+
+// running the app
+app.mount("#app");
