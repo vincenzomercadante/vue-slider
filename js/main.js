6f398afe5906fdca5883f3ca6cdabfb7fe9b287c
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-01-30 16:57:50 +0100

    feat: add slide interval

--- js/main.js
+++ js/main.js
@@ -12,16 +12,27 @@ const app = createApp({
   },
 
   methods: {
-    goToSlide() {
-      if (this.activeImage === this.allImages.length) {
+    changeSlide() {
+      if (this.activeImage >= this.allImages.length) {
         this.activeImage = 0;
       } else if (this.activeImage < 0) {
         this.activeImage = this.allImages.length - 1;
       }
     },
+    startInterval() {
+      this.interval = setInterval(() => {
+        this.changeSlide(this.activeImage++);
+      }, 3000);
+    },
+    stopInterval() {
+      clearInterval(this.interval);
+      this.interval = false;
+    },
   },
 
-  mounted() {},
+  mounted() {
+    this.startInterval();
+  },
 });
 
 // running the app
